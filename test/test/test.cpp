3f122acac0aec0ca6f5c07310a340f5d08a36ae5
Kunyao Lan <lankunyao@sjtu.edu.cn>
2020-06-07 20:59:55 +0800

    Code of Test

---
+++ test/test/test.cpp
@@ -0,0 +1,95 @@
+﻿// test.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#ifndef UNICODE
+#define UNICODE
+#endif
+
+#define WIN32_LEAN_AND_MEAN
+
+#include <winsock2.h>
+#include <Ws2tcpip.h>
+#include <stdio.h>
+
+// Link with ws2_32.lib
+#pragma comment(lib, "Ws2_32.lib")
+
+int main()
+{
+
+    int iResult = 0;
+
+    WSADATA wsaData;
+
+    SOCKET RecvSocket;
+    struct sockaddr_in RecvAddr;
+
+    unsigned short Port = 27015;
+
+    char RecvBuf[1024];
+    int BufLen = 1024;
+
+    struct sockaddr_in SenderAddr;
+    int SenderAddrSize = sizeof(SenderAddr);
+
+    //-----------------------------------------------
+    // Initialize Winsock
+    iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    if (iResult != NO_ERROR) {
+        wprintf(L"WSAStartup failed with error %d\n", iResult);
+        return 1;
+    }
+    //-----------------------------------------------
+    // Create a receiver socket to receive datagrams
+    RecvSocket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    if (RecvSocket == INVALID_SOCKET) {
+        wprintf(L"socket failed with error %d\n", WSAGetLastError());
+        return 1;
+    }
+    //-----------------------------------------------
+    // Bind the socket to any address and the specified port.
+    RecvAddr.sin_family = AF_INET;
+    RecvAddr.sin_port = htons(Port);
+    RecvAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    iResult = bind(RecvSocket, (SOCKADDR*)&RecvAddr, sizeof(RecvAddr));
+    if (iResult != 0) {
+        wprintf(L"bind failed with error %d\n", WSAGetLastError());
+        return 1;
+    }
+    //-----------------------------------------------
+    // Call the recvfrom function to receive datagrams
+    // on the bound socket.
+    wprintf(L"Receiving datagrams...\n");
+    iResult = recvfrom(RecvSocket,
+        RecvBuf, BufLen, 0, (SOCKADDR*)&SenderAddr, &SenderAddrSize);
+    if (iResult == SOCKET_ERROR) {
+        wprintf(L"recvfrom failed with error %d\n", WSAGetLastError());
+    }
+
+    //-----------------------------------------------
+    // Close the socket when finished receiving datagrams
+    wprintf(L"Finished receiving. Closing socket.\n");
+    iResult = closesocket(RecvSocket);
+    if (iResult == SOCKET_ERROR) {
+        wprintf(L"closesocket failed with error %d\n", WSAGetLastError());
+        return 1;
+    }
+
+    //-----------------------------------------------
+    // Clean up and exit.
+    wprintf(L"Exiting.\n");
+    WSACleanup();
+    return 0;
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
---
+++ test2/test2/test2.cpp
@@ -0,0 +1,91 @@
+﻿// test2.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#ifndef UNICODE
+#define UNICODE
+#endif
+
+#define WIN32_LEAN_AND_MEAN
+
+#include <winsock2.h>
+#include <Ws2tcpip.h>
+#include <stdio.h>
+
+// Link with ws2_32.lib
+#pragma comment(lib, "Ws2_32.lib")
+
+int main()
+{
+
+    int iResult;
+    WSADATA wsaData;
+
+    SOCKET SendSocket = INVALID_SOCKET;
+    sockaddr_in RecvAddr;
+
+    unsigned short Port = 27015;
+
+    char SendBuf[1024];
+    int BufLen = 1024;
+
+    //----------------------
+    // Initialize Winsock
+    iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    if (iResult != NO_ERROR) {
+        wprintf(L"WSAStartup failed with error: %d\n", iResult);
+        return 1;
+    }
+
+    //---------------------------------------------
+    // Create a socket for sending data
+    SendSocket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    if (SendSocket == INVALID_SOCKET) {
+        wprintf(L"socket failed with error: %ld\n", WSAGetLastError());
+        WSACleanup();
+        return 1;
+    }
+    //---------------------------------------------
+    // Set up the RecvAddr structure with the IP address of
+    // the receiver (in this example case "192.168.1.1")
+    // and the specified port number.
+    RecvAddr.sin_family = AF_INET;
+    RecvAddr.sin_port = htons(Port);
+    inet_pton(AF_INET, "127.0.0.1", &(RecvAddr.sin_addr));
+
+    //---------------------------------------------
+    // Send a datagram to the receiver
+    wprintf(L"Sending a datagram to the receiver...\n");
+    iResult = sendto(SendSocket,
+        SendBuf, BufLen, 0, (SOCKADDR*)&RecvAddr, sizeof(RecvAddr));
+    if (iResult == SOCKET_ERROR) {
+        wprintf(L"sendto failed with error: %d\n", WSAGetLastError());
+        closesocket(SendSocket);
+        WSACleanup();
+        return 1;
+    }
+    //---------------------------------------------
+    // When the application is finished sending, close the socket.
+    wprintf(L"Finished sending. Closing socket.\n");
+    iResult = closesocket(SendSocket);
+    if (iResult == SOCKET_ERROR) {
+        wprintf(L"closesocket failed with error: %d\n", WSAGetLastError());
+        WSACleanup();
+        return 1;
+    }
+    //---------------------------------------------
+    // Clean up and quit.
+    wprintf(L"Exiting.\n");
+    WSACleanup();
+    return 0;
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
